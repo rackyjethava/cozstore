1a088689e9e20aa5ed1e57f21300aac5433f1382
rackyjethava <raktpx2356@gmail.com>
2024-03-18 08:11:16 +0530

    ----

--- src/user/container/home/Home.js
+++ src/user/container/home/Home.js
@@ -1,91 +1,50 @@
 import React from 'react';
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css'
-
+import Slider from 'react-slick';
+import styles from '../home/Home.module.css';
 function Home(props) {
     const settings = {
         autoplay: true,
-        smartSpeed: 1500,
-        center: false,
+        autoplaySpeed: 1500,
         dots: true,
-        loop: true,
-        margin: 25,
-        nav : true,
-        }
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    }
 
-        
+
 
     return (
         <div>
             {/*Slider */}
             <section className="section-slide">
                 <div className="wrap-slick1">
-                    < OwlCarousel {...settings} className="slick1">
-                        <div className="item-slick1" style={{ backgroundImage: 'url(images/slide-01.jpg)' }}>
-                            <div className="container h-full">
-                                <div className="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-                                    <div className="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay={0}>
-                                        <span className="ltext-101 cl2 respon2">
-                                            Women Collection 2018
-                                        </span>
-                                    </div>
-                                    <div className="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay={800}>
-                                        <h2 className="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                                            NEW SEASON
-                                        </h2>
-                                    </div>
-                                    <div className="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay={1600}>
-                                        <a href="product.html" className="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-                                            Shop Now
-                                        </a>
-                                    </div>
-                                </div>
+                    <Slider {...settings}>
+                        <div className={styles.slideContent}>
+                            <div className={styles.slideText}>
+                                <span>Women Collection 2018</span>
+                                <h2>NEW SEASON</h2>
+                                <a href="product.html">Shop Now</a>
                             </div>
+                            <img src="images/slide-01.jpg" alt="Slide 1" />
                         </div>
-                        <div className="item-slick1" style={{ backgroundImage: 'url(images/slide-02.jpg)' }}>
-                            <div className="container h-full">
-                                <div className="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-                                    <div className="layer-slick1 animated visible-false" data-appear="rollIn" data-delay={0}>
-                                        <span className="ltext-101 cl2 respon2">
-                                            Men New-Season
-                                        </span>
-                                    </div>
-                                    <div className="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay={800}>
-                                        <h2 className="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                                            Jackets &amp; Coats
-                                        </h2>
-                                    </div>
-                                    <div className="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay={1600}>
-                                        <a href="product.html" className="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-                                            Shop Now
-                                        </a>
-                                    </div>
-                                </div>
+                        <div className={styles.slideContent}>
+                            <div className={styles.slideText}>
+                                <span>Men New-Season</span>
+                                <h2>Jackets & Coats</h2>
+                                <a href="product.html">Shop Now</a>
                             </div>
+                            <img src="images/slide-02.jpg" alt="Slide 2" />
                         </div>
-                        <div className="item-slick1" style={{ backgroundImage: 'url(images/slide-03.jpg)' }}>
-                            <div className="container h-full">
-                                <div className="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-                                    <div className="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay={0}>
-                                        <span className="ltext-101 cl2 respon2">
-                                            Men Collection 2018
-                                        </span>
-                                    </div>
-                                    <div className="layer-slick1 animated visible-false" data-appear="rotateInUpRight" data-delay={800}>
-                                        <h2 className="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                                            New arrivals
-                                        </h2>
-                                    </div>
-                                    <div className="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay={1600}>
-                                        <a href="product.html" className="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-                                            Shop Now
-                                        </a>
-                                    </div>
-                                </div>
+                        <div className={styles.slideContent}>
+                            <div className={styles.slideText}>
+                                <span>Men Collection 2018</span>
+                                <h2>New arrivals</h2>
+                                <a href="product.html">Shop Now</a>
                             </div>
+                            <img src="images/slide-03.jpg" alt="Slide 3" />
                         </div>
-                    </OwlCarousel>
+                    </Slider>
                 </div>
             </section>
             {/*Banner */}
--- src/user/container/shop/Shop.js
+++ src/user/container/shop/Shop.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Shop(props) {
+    const [product, setproduct] = useState([])
+    const [category, setCategory] = useState([])
+    const [types, setTypes] = useState([])
+
+const clothData=async()=>{
+    const respnce = await fetch("http://localhost:8000/products");
+    const data = await respnce.json();
+
+    setproduct(data)
+
+}
+
+console.log(product);
+
+useEffect(() => {
+    clothData();
+}, [])
+
     return (
         <div>
             {/* Cart */}
@@ -276,6 +294,37 @@ function Shop(props) {
                         </div>
                     </div>
                     <div className="row isotope-grid">
+                        {
+                            product.map((v)=>{
+                                <div className="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+                                {/* Block2 */}
+                                <div className="block2">
+                                    <div className="block2-pic hov-img0">
+                                        <img src={v.image} alt="IMG-PRODUCT" />
+                                        <a href="#" className="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+                                            Quick View
+                                        </a>
+                                    </div>
+                                    <div className="block2-txt flex-w flex-t p-t-14">
+                                        <div className="block2-txt-child1 flex-col-l ">
+                                            <a href="product-detail.html" className="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                                                {v.name}
+                                            </a>
+                                            <span className="stext-105 cl3">
+                                                {v.price}$
+                                            </span>
+                                        </div>
+                                        <div className="block2-txt-child2 flex-r p-t-3">
+                                            <a href="#" className="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                                                <img className="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON" />
+                                                <img className="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            })
+                    }
                         <div className="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
                             {/* Block2 */}
                             <div className="block2">
